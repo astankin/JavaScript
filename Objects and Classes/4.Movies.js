477c9979c3936cfc1b6e4412f6432046076a93cc
Atanas Stankin <a_stankin@abv.bg>
2023-02-28 08:28:31 +0200

    fix

--- Objects and Classes/4.Movies.js
+++ Objects and Classes/4.Movies.js
@@ -20,10 +20,10 @@ function solve(arr) {
         let data = elem.split(" ");
         let searchedMovieName = "";
         let movie;
-        
+
         if (elem.split(" ")[0] === "addMovie") {
             let movieName = data.slice(1).join(" ");
-            moviesArr.push(new Movie(movieName, null, null));
+            moviesArr.push(new Movie(movieName));
 
         } else if (data.includes("directedBy")) {
             let idx = data.indexOf("directedBy");
@@ -48,7 +48,7 @@ function solve(arr) {
     }
 
     for (const movie of moviesArr) {
-        if (movie.date != null && movie.director != null)
+        if (movie.date && movie.director)
         console.log(JSON.stringify(movie));
     }
 }
