0566bc1741ffe9a2fa3510bede9fa543445afaa4
Atanas Stankin <a_stankin@abv.bg>
2023-03-09 16:53:14 +0200

    update

--- DOM-and-Events-Lab-Resources/JS-Front-End-DOM-and-Events-Exercises-Resources/04. Locked-Profile/app.js
+++ DOM-and-Events-Lab-Resources/JS-Front-End-DOM-and-Events-Exercises-Resources/04. Locked-Profile/app.js
@@ -3,7 +3,7 @@ function lockedProfile() {
     .forEach(btn => btn.addEventListener('click', onClick)); // addEventlistener to every button 
 
     function onClick(event){
-        let profile = event.currrentTarget.parentElement; // get parent to the clicked button
-        let isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked;
+        let profile = event.target.parentElement; // get parent to the clicked button
+        let isActive = profile.querySelector('input[type="radio"][value="unlock"]').checked; // check if radio button is checked
     }
 }
