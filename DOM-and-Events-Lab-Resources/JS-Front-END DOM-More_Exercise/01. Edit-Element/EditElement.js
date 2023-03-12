a54a12fa180746145540430daad3b399811bd5b4
Atanas Stankin <a_stankin@abv.bg>
2023-03-12 09:46:10 +0200

    update JavaScript file

--- DOM-and-Events-Lab-Resources/JS-Front-END DOM-More_Exercise/01. Edit-Element/EditElement.js
+++ DOM-and-Events-Lab-Resources/JS-Front-END DOM-More_Exercise/01. Edit-Element/EditElement.js
@@ -1,5 +1,7 @@
 function editElement(element, matchElement, replacer) {
-  while (element.textContent.includes(matchElement)){
-    element.textContent = element.textContent.replace(matchElement, replacer)
-  }
+  // while (element.textContent.includes(matchElement)){
+  //   element.textContent = element.textContent.replace(matchElement, replacer)
+  // }
+  let pattern = new RegExp(matchElement, 'g');
+  element.textContent = element.textContent.replace(pattern, replacer);
 }
