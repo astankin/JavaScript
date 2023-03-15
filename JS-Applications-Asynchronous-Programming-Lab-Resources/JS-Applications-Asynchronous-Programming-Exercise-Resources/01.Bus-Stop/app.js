519c8141407ddbf1428ccf04fd14a728d8932142
Atanas Stankin <a_stankin@abv.bg>
2023-03-15 12:30:34 +0200

    update

--- JS-Applications-Asynchronous-Programming-Lab-Resources/JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
+++ JS-Applications-Asynchronous-Programming-Lab-Resources/JS-Applications-Asynchronous-Programming-Exercise-Resources/01.Bus-Stop/app.js
@@ -1,13 +1,40 @@
 async function getInfo() {
+<<<<<<< Updated upstream
     const stopInfoElement = document.getElementById('stopId');
     const stopId = stopInfoElement.value;
     const stopNameElement = document.getElementById('stopName');
+=======
+    
+  const stopInfoElement = document.getElementById("stopId");
+  const stopNameElement = document.getElementById("stopName");
+  const busList = document.getElementById("buses");
+  const stopId = stopInfoElement.value;
+  const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+>>>>>>> Stashed changes
 
-    const url = `http://localhost:3030/jsonstore/advanced/articles/details/${stopId}`;
+  busList.innerHTML = "";
+  stopInfoElement.value = "";
 
+  try {
     const response = await fetch(url);
     const data = await response.json();
+<<<<<<< Updated upstream
 ;
     stopNameElement.textContent = data.name;
     debugger
 }
+=======
+
+    stopNameElement.textContent = data.name;
+
+    Object.entries(data.buses).forEach(([busNumber, timeArrive]) => {
+      const li = document.createElement("li");
+      li.textContent = `Bus ${busNumber} arrives in ${timeArrive} minutes`;
+
+      busList.appendChild(li);
+    });
+  } catch (error) {
+    stopNameElement.textContent = "Error";
+  }
+}
+>>>>>>> Stashed changes
---
+++ JS-Applications-Asynchronous-Programming-Lab-Resources/JS-Front-End-HTTP-and-REST-Exercises-Resources (1)/01.Bus-Stop/app.js
@@ -0,0 +1,26 @@
+async function getInfo() {
+    const busStopIdElement = document.getElementById('stopId');
+    let busId = busStopIdElement.value;
+    const stopNameElement = document.getElementById('stopName');
+    const busesElement = document.getElementById('buses');
+
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${busId}`;
+    busStopIdElement.value = '';
+    busesElement.innerHTML = '';
+
+    try {
+        
+        const resp = await fetch(url);
+        const data = await resp.json();
+
+        stopNameElement.textContent = data.name;
+        Object.entries(data.buses).forEach(([busNumber, arrTime]) => {
+            let li = document.createElement('li');
+            li.textContent = `Bus ${busNumber} arrives in ${arrTime} minutes`;
+            busesElement.appendChild(li);
+        })
+
+    } catch (error) {
+        stopNameElement.textContent = 'Error';
+    }
+}
