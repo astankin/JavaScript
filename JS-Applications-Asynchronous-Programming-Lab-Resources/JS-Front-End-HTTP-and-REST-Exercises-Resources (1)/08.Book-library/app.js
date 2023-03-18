fd45a988832d580204aea6c81caaab31f4bfa92a
Atanas Stankin <a_stankin@abv.bg>
2023-03-18 23:22:36 +0200

    Update Book Library

--- JS-Applications-Asynchronous-Programming-Lab-Resources/JS-Front-End-HTTP-and-REST-Exercises-Resources (1)/08.Book-library/app.js
+++ JS-Applications-Asynchronous-Programming-Lab-Resources/JS-Front-End-HTTP-and-REST-Exercises-Resources (1)/08.Book-library/app.js
@@ -1,84 +1,73 @@
 function attachEvents() {
-  document.getElementById('loadBooks').addEventListener('click', getData);
-  document.querySelector('#form button').addEventListener('click', condition);
+  document.getElementById("loadBooks").addEventListener("click", getData);
+  document.querySelector("#form button").addEventListener("click", condition);
 
-  let id1 = '';
-
-  let body = {};
+  let id1 = "";
 
   let isEditBtnClicked = false;
 
-  function condition(e){
-    if (isEditBtnClicked){
-      editBookData(id1, body);
-    }else {
+  function condition(e) {
+    if (isEditBtnClicked) {
+      editBookData(id1);
+    } else {
       createRecord(e);
     }
   }
 
-
-function createRecord(e){
-  e.preventDefault();
-  const titleElement = document.querySelector('input[name="title"]');
-  const authorElement = document.querySelector('input[name="author"]');
-  if (titleElement.value && authorElement.value){
-    let body = {
-      "author": authorElement.value,
-      "title": titleElement.value,
+  function createRecord(e) {
+    e.preventDefault();
+    const titleElement = document.querySelector('input[name="title"]');
+    const authorElement = document.querySelector('input[name="author"]');
+    if (titleElement.value && authorElement.value) {
+      let body = {
+        author: authorElement.value,
+        title: titleElement.value,
+      };
+      postRecord(body);
+      titleElement.value = "";
+      authorElement.value = "";
     }
-    postRecord(body);
-    titleElement.value = '';
-    authorElement.value = '';
   }
-}
 
-async function postRecord(body){
-  const url = 'http://localhost:3030/jsonstore/collections/books';
-        const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        });
-        
-        const data = await response.json();
-        getData();
-}
+  async function postRecord(body) {
+    const url = "http://localhost:3030/jsonstore/collections/books";
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
 
-  let tbody = document.querySelector('tbody');
+    const data = await response.json();
+    getData();
+  }
 
-  async function getData(){
-    const url = 'http://localhost:3030/jsonstore/collections/books';
+  let tbody = document.querySelector("tbody");
+
+  async function getData() {
+    const url = "http://localhost:3030/jsonstore/collections/books";
     const response = await fetch(url);
     const data = await response.json();
     renderData(data);
   }
-  function renderData(data){
-
-    tbody.innerHTML = '';
+  function renderData(data) {
+    tbody.innerHTML = "";
     Object.entries(data).forEach(([id, book]) => {
-      id1 = id;
-      let tr = document.createElement('tr');
+      // id1 = id;
+      let tr = document.createElement("tr");
+      tr.setAttribute('id', id);
       let tdTitleElement = document.createElement("td");
       let tdAuthorElement = document.createElement("td");
       let tdButtonsElement = document.createElement("td");
-      let editBtn = document.createElement('button');
-      editBtn.textContent = 'Edit';
-      editBtn.addEventListener('click', editRecord);
-      function editRecord(){
-        loadEditData(id);
-      }
+      let editBtn = document.createElement("button");
+      editBtn.textContent = "Edit";
+      editBtn.addEventListener("click", loadEditData);
 
-
-      let deleteBtn = document.createElement('button');
-      deleteBtn.textContent = 'Delete';
-      deleteBtn.addEventListener('click', deleteItem)
-      function deleteItem(e){
-        deleteRow(e);
-        deleteRecord(id);
-      }
-
+      let deleteBtn = document.createElement("button");
+      deleteBtn.textContent = "Delete";
+      deleteBtn.addEventListener("click", deleteRow);
+       
       tdTitleElement.textContent = book.title;
       tr.appendChild(tdTitleElement);
 
@@ -91,12 +80,13 @@ async function postRecord(body){
       tr.appendChild(tdButtonsElement);
 
       tbody.appendChild(tr);
-
-    })
+    });
   }
 
-  async function loadEditData(id){
-    // console.log(id);
+  async function loadEditData(e) {
+    let tr = e.currentTarget.parentElement.parentElement;
+    let id = tr.id;
+    id1 = id;
     isEditBtnClicked = true;
 
     const url = `http://localhost:3030/jsonstore/collections/books`;
@@ -106,48 +96,48 @@ async function postRecord(body){
     const authorElement = document.querySelector('input[name="author"]');
     titleElement.value = data[id].title;
     authorElement.value = data[id].author;
-    
   }
 
-  async function editBookData(id){
+  async function editBookData(id) {
+    const titleElement = document.querySelector('input[name="title"]');
+    const authorElement = document.querySelector('input[name="author"]');
+    if (titleElement.value && authorElement.value) {
+      let body = {
+        author: authorElement.value,
+        title: titleElement.value,
+      };
+      titleElement.value = "";
+      authorElement.value = "";
 
-  const titleElement = document.querySelector('input[name="title"]');
-  const authorElement = document.querySelector('input[name="author"]');
-  if (titleElement.value && authorElement.value){
-    body = {
-      "author": authorElement.value,
-      "title": titleElement.value,
+      const url = `http://localhost:3030/jsonstore/collections/books/${id}`;
+      const response = await fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+
+      const data = await response.json();
+      console.log(data);
+      isEditBtnClicked = false;
+      getData();
     }
-    titleElement.value = '';
-    authorElement.value = '';
-
-    const url = `http://localhost:3030/jsonstore/collections/books/${id}`;
-        const response = await fetch(url, {
-            method: 'PUT',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: JSON.stringify(body)
-        });
-        
-        const data = await response.json();
-        console.log(data);
-        isEditBtnClicked = false;
-        getData();
-  }
   }
 
-  function deleteRow(e){
+  function deleteRow(e) {
     let tr = e.currentTarget.parentElement.parentElement;
+    let id = tr.id;
     tr.remove();
+
+    deleteRecord(id);
   }
-  async function deleteRecord(id){
+  async function deleteRecord(id) {
     const url = `http://localhost:3030/jsonstore/collections/books/${id}`;
     const response = await fetch(url, {
-        method: 'DELETE'
+      method: "DELETE",
     });
-}
-
+  }
 }
 
 attachEvents();
