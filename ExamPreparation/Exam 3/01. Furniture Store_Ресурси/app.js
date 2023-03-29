502934ae971d3647729bd2c42d5a1faa198c45a3
Atanas Stankin <a_stankin@abv.bg>
2023-03-29 11:41:52 +0300

    Fixing bug

--- ExamPreparation/Exam 3/01. Furniture Store_Ресурси/app.js
+++ ExamPreparation/Exam 3/01. Furniture Store_Ресурси/app.js
@@ -20,7 +20,15 @@ function solve() {
         let description = descriptionElement.value;
         let price = Number(priceElement.value);
 
-        //TO DO CHECK IF NUMBER AND EMPTY FIELD
+        if (!model || !description || !year || !price){
+           return;  
+        }
+        if (Number(year) < 0){
+            return;
+        }
+        if (Number(price) < 0){
+            return;
+        }
 
         let tr1 = createElement('tr', '', tbody, 'info');
         createElement('td', model, tr1);
@@ -28,26 +36,56 @@ function solve() {
 
         let tdBtn = createElement('td', '', tr1);
         let moreInfoBtn = createElement('button', 'More Info', tdBtn, 'moreBtn');
-        moreInfoBtn.addEventListener('click', moreInfoHandler);
+        moreInfoBtn.addEventListener('click', (e) => {
+            if (e.currentTarget.textContent == 'More Info'){
+                moreInfoHandler(e);
+            } else {
+                lessInfoHandler(e);
+            }
+        });
 
         let buyBtn = createElement('button', 'Buy it', tdBtn, 'buyBtn');
+        buyBtn.addEventListener('click', buyItem);
 
         let tr2 = createElement('tr', '', tbody, 'hide');
         createElement('td', `Year: ${year}`, tr2);
         createElement('td', `Description: ${description}`, tr2);
 
+        modelElement.value = '';
+        yearElement.value = '';
+        descriptionElement.value = '';
+        priceElement.value = '';
+
+    }
+
+    function buyItem(e){
+        let tr = e.currentTarget.parentElement.parentElement;
+        let price = Number(tr.querySelectorAll('td')[1].textContent);
+        console.log(price);
+        tr.nextSibling.remove();
+        tr.remove();
+        let totalPriceElement = document.querySelector('.total-price');
+        let totalPrice = Number(totalPriceElement.textContent);
+        totalPriceElement.textContent = (totalPrice + price).toFixed(2)
+        
     }
 
     function moreInfoHandler(e){
-        let moreInfoBtn = e.currentTarget;
-        moreInfoBtn.textContent = 'Les info';
-        let parent = e.currentTarget.parentElement.parentElement.parentElement;
-        console.log(parent);
-        let hideTr = parent.querySelector('.hide');
+        
+        let parent = e.currentTarget.parentElement.parentElement;
+        e.currentTarget.textContent = 'Less Info';
+        
+        let hideTr = parent.nextSibling;
         hideTr.style.display = "contents";
-        let td = hideTr.querySelectorALL('td')[1];
+        let td = hideTr.querySelectorAll('td')[1];
         td.setAttribute('colspan', 3);
 
+    }
+    function lessInfoHandler(e){
+        e.currentTarget.textContent = 'More Info';
+        let grandpParent = e.currentTarget.parentElement.parentElement;
+        let hideTr = grandpParent.nextSibling;
+        hideTr.style.display = "none";
     }
 
     function createElement(type, content, parent, className){
